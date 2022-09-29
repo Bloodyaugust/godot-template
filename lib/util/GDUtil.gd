0e01307f26a94ce230c5074f9bec2042a81bb3e2
Bloodyaugust <greysonrichey@gmail.com>
2022-09-28 23:51:30 -0600

    Update for Godot 3.5.1
    - Add get_files_in_directory() to GDUtil

--- lib/util/GDUtil.gd
+++ lib/util/GDUtil.gd
@@ -13,6 +13,24 @@ static func centroid(points:Array) -> Vector2:
 
   return centroid
 
+static func get_files_in_directory(directory_path: String) -> Array:
+  var _directory: Directory = Directory.new()
+  var _files: Array = []
+
+  if _directory.open(directory_path) == OK:
+    _directory.list_dir_begin(true)
+    var _file_name: String = _directory.get_next()
+
+    while _file_name != "":
+      if !_directory.current_is_dir():
+        _files.append(_file_name)
+
+      _file_name = _directory.get_next()
+
+    _directory.list_dir_end()
+
+  return _files
+
 static func free_children(node):
   for n in node.get_children():
       n.free()
