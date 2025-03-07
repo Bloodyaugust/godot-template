c4ac272a8c57ec15808b0e1c69b594b25d7f10a7
Bloodyaugust <greysonrichey@gmail.com>
2025-03-07 14:21:13 -0700

    Update for v4.4-stable
    Improvements for GDUtil

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # godot-template
 
-An opinionated template for Godot projects. Currently, this branch is up-to-date with v4.4-beta1
+An opinionated template for Godot projects. Currently, this branch is up-to-date with v4.4-stable
 
 Following the structure provided by this template can speed up development, especially for jams or prototypes. It's probably not enough for larger projects, but can serve as a good starting point.
 
--- lib/util/GDUtil.gd
+++ lib/util/GDUtil.gd
@@ -43,43 +43,70 @@ static func reference_safe(object: Variant) -> bool:
 	return object != null && !object.is_queued_for_deletion() && is_instance_valid(object)
 
 
-static func tilemap_global_cell_position(tilemap: TileMap, position: Vector2) -> Vector2:
+static func tilemap_global_cell_position(tilemap: TileMapLayer, position: Vector2) -> Vector2:
 	return tilemap.to_global(tilemap.map_to_world(tilemap.world_to_map(tilemap.to_local(position))))
 
 
-## Returns all files in a directory. Useful for data-driven games.
-static func load_directory(directory_path: String) -> Array[Variant]:
-	var _directory: DirAccess = DirAccess.open(directory_path)
-	var _paths: Array[String] = []
-	var _loads: Array = []
+## Returns a dictionary where the key is a Resource, and the value is the path it was loaded from.
+##
+## Recursively loads folders in the path. Useful for data-driven games.
+static func load_resources_directory(directory_path: String) -> Dictionary[Resource, String]:
+	var directory: DirAccess = DirAccess.open(directory_path)
+	var resources: Dictionary[Resource, String] = {}
 
-	if _directory:
-		_directory.list_dir_begin()
-		var _file_name: String = _directory.get_next()
+	if directory:
+		directory.list_dir_begin()
+		var file_name: String = directory.get_next()
 
-		while _file_name != "":
-			if !_directory.current_is_dir():
-				_paths.append(directory_path + _file_name)
+		while file_name != "":
+			if !directory.current_is_dir():
+				if file_name.ends_with(".tres"):
+					var resource_path: String = directory_path + file_name
+					var new_resource: Resource = ResourceLoader.load(resource_path)
+					
+					resources[new_resource] = resource_path
+			else:
+				var inner_resources: Dictionary[Resource, String] = load_resources_directory(directory_path + file_name + "/")
+				
+				for inner_resource in inner_resources:
+					resources[inner_resource] = inner_resources[inner_resource]
 
-			_file_name = _directory.get_next()
+			file_name = directory.get_next()
 
-		for _path in _paths:
-			_loads.append(load(_path))
-
-	return _loads
+	return resources
 
 
-static func get_tile_from_global_position(position: Vector2, tilemap: TileMap) -> Vector2i:
+static func get_camera_zoom_contain_sprite(camera: Camera2D, sprite: Sprite2D, padding: float) -> Vector2:
+	var bounding_rect: Rect2 = sprite.get_rect() * sprite.global_transform
+	
+	var viewport_size: Vector2 = camera.get_viewport().size
+	var scale_x: float = (viewport_size.x - (padding * 2)) / bounding_rect.size.x
+	var scale_y: float = (viewport_size.y - (padding * 2)) / bounding_rect.size.y
+	var needed_scale: float = min(scale_x, scale_y)
+	
+	return Vector2(needed_scale, needed_scale)
+
+
+static func get_tile_from_global_position(position: Vector2, tilemap: TileMapLayer) -> Vector2i:
 	return tilemap.local_to_map(tilemap.to_local(position))
 
 
 static func get_tile_from_offset_global(
-	position: Vector2, offset: Vector2, tilemap: TileMap
+	position: Vector2, offset: Vector2, tilemap: TileMapLayer
 ) -> Vector2i:
 	return get_tile_from_global_position(
 		position + (offset * Vector2(tilemap.tile_set.tile_size)), tilemap
 	)
 
 
-static func get_global_position_from_tile(tile: Vector2i, tilemap: TileMap) -> Vector2:
+static func get_global_position_from_tile(tile: Vector2i, tilemap: TileMapLayer) -> Vector2:
 	return tilemap.to_global(tilemap.map_to_local(tile))
+
+
+static func rotate_towards(node: Node2D, target_angle: float, step: float) -> void:
+	var difference: float = angle_difference(node.rotation, target_angle)
+
+	if abs(difference) <= step:
+		node.rotate(difference)
+	else:
+		node.rotate(sign(difference) * step)
